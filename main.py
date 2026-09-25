6869979840c9b85eccc4561f427348008723679f
JijackOoLantern <aizatmuqriz00@gmail.com>
2026-09-26 02:21:02 +0800

    Reduce personal queue duration from 180 to 90

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ TIER_CONFIG = {
         'name': 'Normal User (Default)',
         'link_cooldown': 14400,   
         'photo_cooldown': 14400,  
-        'personal_queue_duration': 180,      
+        'personal_queue_duration': 90,      
         'delete_cooldown': 60,  
         'delete_access': 'own',
         'price': 0,
